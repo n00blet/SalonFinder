1b82194ae89a891e8b0e98cba257775b772abd6d
Rakshith <pritam.rakshi@gmail.com>
2015-06-08 17:55:30 +0530

    dynamic location based salons

--- app/src/main/java/com/salon/finder/ui/MainActivity.java
+++ app/src/main/java/com/salon/finder/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.salon.finder.ui;
 
 import android.app.ProgressDialog;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,7 +10,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.salon.finder.R;
@@ -27,12 +34,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private DateTime dateTime;
     private ArrayList<SalonObjects> salons;
     private ListView listView;
     private CustomAdapter adapter;
+    private Location mLastLocation;
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+    // boolean flag to toggle periodic location updates
+    private boolean mRequestingLocationUpdates = false;
+    private LocationRequest mLocationRequest;
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,34 +55,61 @@ public class MainActivity extends AppCompatActivity{
         dateTime = DateTime.now();
         salons = new ArrayList<>();
         listView = (ListView) findViewById(R.id.salon_list);
-        String data[] = dateTime.toLocalDate().toString().split("-");
-        Log.d("Data", ":" + data);
-        getNearByFourSquareSalon("20150607", "12.96,77.56");
+
+        if (checkPlayServices()) {
+            buildGoogleApiClient();
+        }
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.d("Position",":" + position);
+                Log.d("Position", ":" + position);
             }
         });
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
 
 
+    private void makeApiRequests(String ll){
+        getNearByFourSquareSalon("20150608",ll);
+    }
 
-
-    private void getNearByFourSquareSalon(String version,String location){
-        HashMap<String,String> paramMap = new HashMap<>();
-        paramMap.put("client_id",AppConstants.FOURSQUARE_CLIENT);
-        paramMap.put("client_secret",AppConstants.FOURSQUARE_CLIENT_TOKEN);
-        paramMap.put("query",AppConstants.SEARCH_QUERY);
-        paramMap.put("v",version);
+    private void getNearByFourSquareSalon(String version, String location) {
+        Log.d("Api call", "true");
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put("client_id", AppConstants.FOURSQUARE_CLIENT);
+        paramMap.put("client_secret", AppConstants.FOURSQUARE_CLIENT_TOKEN);
+        paramMap.put("query", AppConstants.SEARCH_QUERY);
+        paramMap.put("v", version);
         paramMap.put("ll", location);
         RequestParams params = new RequestParams(paramMap);
-        Log.d("Params",":" + params);
-        SalonRestClient.post(AppConstants.FOURSQUARE_BASE_URL,params,new JsonHttpResponseHandler(){
+        Log.d("Params", ":" + params);
+        SalonRestClient.post(AppConstants.FOURSQUARE_BASE_URL, params, new JsonHttpResponseHandler() {
 
             ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+
             @Override
             public void onStart() {
                 super.onStart();
@@ -78,33 +119,32 @@ public class MainActivity extends AppCompatActivity{
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                Log.d("Response",":" + response);
+                Log.d("Response", ":" + response);
                 try {
-                     JSONObject re = response.getJSONObject("response");
-                     JSONArray ar = re.getJSONArray("venues");
-                     int len = ar.length();
-                    for(int i=0;i<len;i++){
+                    JSONObject re = response.getJSONObject("response");
+                    JSONArray ar = re.getJSONArray("venues");
+                    int len = ar.length();
+                    for (int i = 0; i < len; i++) {
                         SalonObjects salon = new SalonObjects();
                         JSONObject location = ar.getJSONObject(i).getJSONObject("location");
                         JSONArray address = location.getJSONArray("formattedAddress");
-                        Log.d("Address",":" + address);
-                        Log.d("Location",":" + location);
+                        Log.d("Address", ":" + address);
+                        Log.d("Location", ":" + location);
                         salon.setLocation(location.toString());
                         salon.setName(address.toString());
                         salons.add(salon);
                     }
 
-                }catch (Exception e){
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
 
-                if(salons.size()>0){
-                    adapter = new CustomAdapter(MainActivity.this,salons);
+                if (salons.size() > 0) {
+                    adapter = new CustomAdapter(MainActivity.this, salons);
                     listView.setAdapter(adapter);
                 }
 
             }
-
 
 
             @Override
@@ -115,7 +155,7 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onFinish() {
                 super.onFinish();
-                if(progressDialog!=null && progressDialog.isShowing())
+                if (progressDialog != null && progressDialog.isShowing())
                     progressDialog.dismiss();
             }
         });
@@ -144,6 +184,67 @@ public class MainActivity extends AppCompatActivity{
     }
 
 
+    /**
+     * Creating google api client object
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
 
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
 
+    private String getLattLong() {
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+        String ll = null;
+        if (mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLongitude();
+            ll = latitude + "," + longitude;
+        } else {
+            ll = getString(R.string.no_address_found);
+        }
+        Log.d("LL", ":::" + ll);
+        return ll;
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        String location_response = getLattLong();
+        if (location_response.equals(getString(R.string.no_address_found)))
+            Toast.makeText(getApplicationContext(), getString(R.string.no_address_found), Toast.LENGTH_SHORT).show();
+        else
+            makeApiRequests(location_response);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d("Failure", ":" + connectionResult.getErrorCode());
+    }
 }
